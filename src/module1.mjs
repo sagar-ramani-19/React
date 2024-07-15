44fba83638e718625d08b5360818ce5d74d11904
sagar-ramani-19 <sagarramani720@gmail.com>
2024-07-15 21:06:03 +0530

    react second change

---
+++ src/module1.mjs
@@ -0,0 +1,10 @@
+const a = "sagar";
+const b = "nirav";
+const c = "viraj";
+const d = "nishant";
+
+// export default d;
+export { a, b, c, d};
+// export { b };
+// export { c };
+// export { d };
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+import { a, b, c, d } from "./module1.mjs";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
